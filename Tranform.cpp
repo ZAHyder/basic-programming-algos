b92952b619d102900322d9bd69847a43caf5f546
ah031371 <160019240+ah031371@users.noreply.github.com>
2025-12-15 03:25:26 +0300

    print function added, transform_inline function corrected, made the program generic

--- Tranform.cpp
+++ Tranform.cpp
@@ -8,24 +8,45 @@
 
 using namespace std;
 
-template<typename Func>
-void transform_inline (vector<float>& container, const Func& func) {
-    for (auto it = container.begin(); it != container.end(); it++) {
+// template<typename Container>
+void print(auto& v) {
+    for (const auto& item : v) {
+        cout << item << ", ";
+    }
+    cout << endl;
+}//auto does the same thing as template
+
+void transform_inline(auto& container, const auto& func) {
+ for (auto it = container.begin(); it != container.end(); it++) {
         auto& item = *it;
         //pointing to an element in vector<float> instead of copying it
-        func(item);
+        item = func(item);
     }
-
 }//transform inline (change the contents of the current container)
 
-int main(int argc, char* argv[]) {
+template<typename Container>
+Container transform(const Container& container, const auto& func) {
+    auto new_cont = Container();
+    for (auto it = container.begin(); it != container.end(); it++) {
+        auto& item = *it;
+        new_cont.push_back(func(item));
+    }
+    return new_cont;
+}//creates new copy of the container with transformed elements
 
+int main(int argc, char* argv[]) {
     auto v = vector<float>{1.1f, 2.2f, 3.3f};
     /* the content of the vector is stored in Heap whereas the attributes of the vector
     are stored in stack, one of those attributes points to the stored contents in Heap */
 
-    transform_inline(v, [/* no attr */](float& item) {item * 10; });
+    print(v);
+    transform_inline(v, [/* no attr */](float& item) {return item * 10; });
     //this is a very small function, its footprint is pretty much 0 this is why we don't need to symbolically link it in the algorithm
     //but since we will not always know the size of the function, better to link it symbolically
+    print(v);
+
+    auto new_cont = transform(v, [](const float& item) {return item * 10; });
+    print(new_cont);
+
     return 0;
 }
